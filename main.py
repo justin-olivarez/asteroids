7fcbaf6ade7b6487fba8c3fde2d687b39095d1d3
Justin Olivarez <jolivarez@teamraft.com>
2025-05-04 17:55:54 -0500

    Add framerate and lock in at 60 FPS

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        # print(dt)
 
 if __name__ == "__main__":
     main()
