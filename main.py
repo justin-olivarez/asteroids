6ca736082f85940ade76c1b89f5ad3f28533ab3c
Justin Olivarez <jolivarez@teamraft.com>
2025-05-06 17:28:18 -0500

    Create player and add spaceship

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
